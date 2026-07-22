9e308368b2460a44327e3f479b52246fe087c205
lenonjonas-rgb <lenonjonas@gmail.com>
2026-07-21 21:24:07 -0300

    Corrige dependencias para flet 0.23.2

--- main.py
+++ main.py
@@ -414,9 +414,11 @@ def main(page: ft.Page):
         )
     )
 
-app = ft.app(target=main, export_to_web=True)
-
 if __name__ == "__main__":
-    import uvicorn
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    ft.app(
+        target=main, 
+        view=ft.AppView.WEB_BROWSER, 
+        port=port, 
+        host="0.0.0.0"
+    )
